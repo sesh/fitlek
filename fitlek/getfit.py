1340719c79d163ebffd40285d26257e19690a397
Brenton Cleeland <brenton@brntn.me>
2023-02-06 13:38:31 +1100

    chore: formatting updates

--- fitlek/getfit.py
+++ fitlek/getfit.py
@@ -8,12 +8,19 @@ GETFIT_URL = "https://getfitfile.azurewebsites.net/api/getfitfromjson"
 def getfit_download(workout_name, workout, save=True):
     j = {
         "name": workout_name,
-        "steps": [{
-            "intensity": step.step_type,
-            "duration": int(step.parsed_end_condition_value()),
-            "targetSpeedLow": step.target.from_value if step.target.from_value else None,
-            "targetSpeedHigh": step.target.to_value if step.target.to_value else None
-        } for step in workout.workout_steps]
+        "steps": [
+            {
+                "intensity": step.step_type,
+                "duration": int(step.parsed_end_condition_value()),
+                "targetSpeedLow": step.target.from_value
+                if step.target.from_value
+                else None,
+                "targetSpeedHigh": step.target.to_value
+                if step.target.to_value
+                else None,
+            }
+            for step in workout.workout_steps
+        ],
     }
 
     response = request(GETFIT_URL, json=j, method="POST")
